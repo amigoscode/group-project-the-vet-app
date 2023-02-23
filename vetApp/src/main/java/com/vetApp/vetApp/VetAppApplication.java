06526d149f79edac5b2a91262d821ce34959a62b
Simon Bruce <simonbrucepuma@gmail.com>
2023-02-23 17:32:33 -0600

    first commit, project initialization

---
+++ vetApp/src/main/java/com/vetApp/vetApp/VetAppApplication.java
@@ -0,0 +1,13 @@
+package com.vetApp.vetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetAppApplication.class, args);
+	}
+
+}
---
+++ vetApp/src/test/java/com/vetApp/vetApp/VetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetApp.vetApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
